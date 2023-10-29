8e30d3a14d10e8b5266d8998db48ac9e47baa023
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-10-29 13:10:52 +0530

    Linear Search

---
+++ Array/2. Linear-Search.py
@@ -0,0 +1,22 @@
+# In this approach we go through each and every element one by one to find the the element
+import array
+
+def LinearSearch(arr, value):
+    for i in range(len(arr)):
+        if(arr[i]==value):
+            return i
+    return -1
+
+s=int(input("Enter the size of the array : "))
+l=[]
+for j in range(s):
+    l.append(int(input(f"Enter value {j}:")))
+a=array.array("i",l)
+
+m=int(input("Enter a value to be searched: "))
+
+l=LinearSearch(a,m)
+if(l!=-1):
+    print(f"Searched avlue is at {l} position in the array")
+else:
+    print("Element not found")
