aef3e4ba52db6fc9de3d3696e8e80ace16b032d5
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-11-01 22:49:35 +0530

    Merge Sort

--- Array/9. Merge-Sort.py
+++ Array/9. Merge-Sort.py
@@ -1,36 +1,31 @@
 import array
 
 def MergeSort(arr):
-    if len(arr) > 1:
-        mid = len(arr)//2              # Finding the mid of the array
-        L = arr[:mid]                  # Dividing the array elements         
-        R = arr[mid:]                  # Into 2 halves
-         
-        MergeSort(L)                   # Sorting the first half
-        MergeSort(R)                   # Sorting the second half
- 
-        i = j = k = 0
- 
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i += 1
+    if(len(arr)>1):
+        mid=len(arr)//2      # to find the middle index of the array
+        L=arr[:mid]
+        R=arr[mid:]
+        MergeSort(L)    # to sort the left half array
+        MergeSort(R)    # to sort the right half array
+
+        i=j=k=0
+        while(i<len(L) and j<len(R)):     # to merge the arrays upto the length of smaller array 
+            if(L[i]<=R[j]):
+                arr[k]=L[i]
+                i=i+1
             else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
- 
-        # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
- 
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
+                arr[k]=R[j]
+                j=j+1
+            k+=1
+            
+        while(i<len(L)):       # to merge any remaining elements in left array
+            arr[k]=L[i]
+            i+=1
+            k=k+1
+        while(j<len(R)):      # to merge any remaining elements in right array
+            arr[k]=R[j]
+            j+=1
+            k+=1
 
 a=array.array("i",[1,7,2,9,5,2,8,4,6])
 MergeSort(a)
